9fc8f59f130d08787644c21c5cb46e111f5a3e59
woo3145 <lcwoo3145@gmail.com>
2024-01-10 20:04:50 +0900

    ✨ StudyCategory Entity - Common Entity 상속

--- src/study/entities/study-category.entity.ts
+++ src/study/entities/study-category.entity.ts
@@ -1,9 +1,10 @@
 import { IsBoolean, IsString } from 'class-validator';
+import { CommonEntity } from 'src/common/entities/common.entity';
 import { User } from 'src/users/entities/user.entity';
 import { Entity, Column, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
 
 @Entity()
-export class StudyCategory {
+export class StudyCategory extends CommonEntity {
   @PrimaryGeneratedColumn()
   id: string;
 
