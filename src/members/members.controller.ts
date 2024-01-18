c7a4f7aa2bd78cac0f7947107588fa6dff64fed4
woo3145 <lcwoo3145@gmail.com>
2024-01-18 17:47:20 +0900

    ✨ Update StudyCategory API 작업

--- src/members/members.controller.ts
+++ src/members/members.controller.ts
@@ -8,12 +8,14 @@ import {
   HttpCode,
   Param,
   ParseIntPipe,
+  Patch,
   Post,
   Req,
 } from '@nestjs/common';
 import { MembersService } from './members.service';
 import { StudyCategoriesService } from 'src/study-categories/study-categories.service';
 import { CreateStudyCategoryDto } from 'src/study-categories/dtos/create-study-category.dto';
+import { UpdateStudyCategoryDto } from 'src/study-categories/dtos/update-study-category.dto';
 
 @Controller('members')
 export class MembersController {
@@ -111,6 +113,40 @@ export class MembersController {
     return {
       status: 204,
       data: result,
+    };
+  }
+
+  @HttpCode(200)
+  @Patch(':member_id/study-categories/:study_category_id')
+  async updateCategory(
+    @Req() req,
+    @Param('member_id') member_id: string,
+    @Param('study_category_id', ParseIntPipe) study_category_id: number,
+    @Body() updateStudyCategoryDto: UpdateStudyCategoryDto,
+  ) {
+    const member = await this.membersService.findOneByAccountId(req.user.sub);
+    if (!member) {
+      throw new BadRequestException(
+        '로그인한 사용자의 프로필이 존재하지 않습니다.',
+      );
+    }
+
+    if (member_id !== member.member_id) {
+      throw new ForbiddenException('카테고리를 수정할 권한이 없습니다.');
+    }
+
+    const result = await this.studyCategoriesService.update(
+      member.member_id,
+      study_category_id,
+      updateStudyCategoryDto,
+    );
+    // eslint-disable-next-line
+    const { hidden: _, ...rest } = result;
+    return {
+      status: 200,
+      data: {
+        study_category: rest,
+      },
     };
   }
 }
--- src/study-categories/study-categories.service.ts
+++ src/study-categories/study-categories.service.ts
@@ -4,6 +4,7 @@ import { MembersService } from 'src/members/members.service';
 import { StudyCategory } from './entities/study-category.entity';
 import { Repository } from 'typeorm';
 import { CreateStudyCategoryDto } from './dtos/create-study-category.dto';
+import { UpdateStudyCategoryDto } from './dtos/update-study-category.dto';
 
 @Injectable()
 export class StudyCategoriesService {
@@ -77,5 +78,55 @@ export class StudyCategoriesService {
     await this.studyCategoryRepository.save(exist);
 
     return null;
+  }
+
+  async update(
+    member_id: string,
+    study_category_id: number,
+    { subject }: UpdateStudyCategoryDto,
+  ) {
+    const exist = await this.studyCategoryRepository.findOne({
+      where: {
+        study_category_id,
+      },
+    });
+    if (!exist) {
+      throw new BadRequestException('해당 카테고리가 존재하지 않습니다.');
+    }
+
+    if (exist.subject === subject) {
+      throw new BadRequestException('변경된 내용이 없습니다.');
+    }
+
+    const targetExist = await this.studyCategoryRepository.findOne({
+      where: {
+        subject,
+        member: {
+          member_id,
+        },
+      },
+      relations: {
+        study_records: true,
+      },
+    });
+
+    if (targetExist) {
+      // exist 공부기록 target으로 합친 후 exist 하드 삭제
+
+      // *** 합치기는 StudyRecordsService 메소드로 처리 (예정) ***
+
+      // target이 소프트 삭제 된 경우 되살리기
+      if (targetExist.hidden) {
+        targetExist.hidden = false;
+        await this.studyCategoryRepository.save(targetExist);
+      }
+      await this.studyCategoryRepository.delete(exist.study_category_id); // 하드 삭제
+      return targetExist;
+    }
+    // target 기록이 없으면 subject 업데이트
+    exist.subject = subject;
+    await this.studyCategoryRepository.save(exist);
+
+    return exist;
   }
 }
